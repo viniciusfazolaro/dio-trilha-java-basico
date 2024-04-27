3f93312c2e1de079834641613d8879ac10690d80
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2024-04-27 20:20:17 -0300

    feat: add ContaTerminal.java file

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int accountNumber = 0;
+        String agency = "";
+        String customerName = "";
+        double balance = 0.0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        customerName = sc.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        agency = sc.nextLine();
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        accountNumber = sc.nextInt();
+
+        System.out.print("Por favor, digite o Saldo: ");
+        balance = sc.nextDouble();
+
+        sc.close();
+
+        System.out.println("Olá " + customerName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + accountNumber + " e seu saldo " + balance + " já está disponível para saque");
     }
 }
